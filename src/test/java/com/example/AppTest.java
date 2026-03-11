d85a308ab5cbd4c1c221d2338f19532fa1ec4ee9
jastisri2023-eng <jasti.sri2023@vitstudent.ac.in>
2026-03-11 11:47:18 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,31 +1,10 @@
-package com.example;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class AppTest {
-
-    @Test
-    public void testAdd() {
-        App app = new App();
-        assertEquals(5, app.add(2,3));
-    }
-
-    @Test
-    public void testSubtract() {
-        App app = new App();
-        assertEquals(3, app.subtract(5,2));
-    }
-
-    @Test
-    public void testMultiply() {
-        App app = new App();
-        assertEquals(6, app.multiply(2,3));
-    }
-
-    @Test
-    public void testDivide() {
-        App app = new App();
-        assertEquals(2, app.divide(6,3));
-    }
-}
+package com.example; 
+import org.junit.Test; 
+import static org.junit.Assert.*; 
+public class AppTest { 
+@Test 
+public void testAdd() { 
+App app = new App(); 
+assertEquals(5, app.add(2, 3)); 
+} 
+} 
