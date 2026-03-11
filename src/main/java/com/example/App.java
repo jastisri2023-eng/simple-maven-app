39f5683356c7fdaa9eaed8a846350329ece4e133
jastisri2023-eng <jasti.sri2023@vitstudent.ac.in>
2026-03-11 11:48:58 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,20 +1,6 @@
-package com.example;
-
-public class App {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int divide(int a, int b) {
-        return a / b;
-    }
-}
+package com.example; 
+public class App { 
+public int add(int a, int b) { 
+return a + b; 
+} 
+} 
